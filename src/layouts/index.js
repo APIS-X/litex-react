2394d23fc778cd52c23c7dbb2072b059ea7c51e0
weiwei.guo <weiwei.guo@tongdun.net>
2023-02-23 09:18:50 +0800

    feat: 路由监听以及参数获取

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 const PageLayout = ({ children }) => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    console.log('pathname', pathname);
+  }, [pathname]);
+
   return <section>{children}</section>;
 };
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { Link, useLocation, useSearchParams } from 'react-router-dom';
 
 const PageHome = () => {
-  return <section>这是首页</section>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const location = useLocation();
+
+  useEffect(() => {
+    const currentParams = Object.fromEntries([...searchParams]);
+    console.log('location', location, searchParams, currentParams);
+  }, []);
+
+  return (
+    <section>
+      这是首页
+      <Link to='/user'>跳转到用户信息</Link>
+    </section>
+  );
 };
 
 export default PageHome;
