2484a938d715ba611144f0f85ddd31b1fd63a4af
APIS <971888462@qq.com>
2023-03-09 14:34:42 +0800

    feat: layout 相关搭建

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import Layout from './layouts';
+import Layout from './layout';
 import { Router } from '@/routers';
 
 import './styles/global.less';
